3edd4c0f7bba8f14e6d8c81cd6546dbf0d640733
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-12-02 17:25:04 -0500

    Homework started.

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var temperature: Double = 15.00
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        VStack (alignment: .leading, spacing: 20) {
+        
+        Text("Temperature")
+                .bold()
+        
+        
+        Slider(value: .constant(50.0),
+               in: -50.0...50.0,
+               step: 1.0,
+               label: {
+                    Text("Temperature")
+        },
+               minimumValueLabel: {
+                    Text("-50")
+        },
+               maximumValueLabel: {
+                    Text("50")
+        })
+        
+ 
+    
+    
+    
+    
+    
+        }
+        .navigationTitle("Temperature Converter")
+        .navigationBarTitleDisplayMode(.inline)
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView {
         ContentView()
+        }
     }
 }
