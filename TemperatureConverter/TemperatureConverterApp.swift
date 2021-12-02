063e67282e4af760e4182eef00f3abcf5b5abd99
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-12-02 17:07:03 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Berk Bozkurt on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
